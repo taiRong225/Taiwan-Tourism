3a3b3956fa0e856e0d8443f6489c4354abd8f5e4
taiRong225 <neribaby95260@gmail.com>
2021-11-23 11:51:45 +0800

    加入尚無圖片

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,11 @@ export const environment = {
   baseURLv2: 'https://ptx.transportdata.tw/MOTC/v2',
   APP_ID: '3c27fa835a8c492eaa6d21b89d9ab351',
   APP_KEY: 'YauXGr2vbJtXzRDgJcTr7A2rX6w',
-  noProvideCity: '未提供縣市'
+  noProvideCity: '未提供縣市',
+  noImage160x160: './assets/images/NoImage-160x160.png',
+  noImage250x200: './assets/images/NoImage-255x200.png',
+  noImage345x160: './assets/images/NoImage-345x160.png',
+  noImage1100x400: './assets/images/NoImage-1100x400.png',
 };
 
 /*
