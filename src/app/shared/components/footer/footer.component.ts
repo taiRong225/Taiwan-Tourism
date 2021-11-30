d52e32bcd0f5057f3dfbaf4e9a77298c6fb39070
taiRong225 <neribaby95260@gmail.com>
2021-11-30 16:28:29 +0800

    頁尾Footer加入動畫效果

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { fade } from 'src/app/packages/animations';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
+  animations: [
+    fade
+  ]
 })
 export class FooterComponent implements OnInit {
 
