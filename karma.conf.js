9a17d505ce9597aca4d96ac3c72f646736fe9862
taiRong225 <neribaby95260@gmail.com>
2021-12-02 11:50:30 +0800

    修正專案名稱拼字錯誤

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/Taiwan-Toursim'),
+      dir: require('path').join(__dirname, './coverage/Taiwan-Tourism'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
