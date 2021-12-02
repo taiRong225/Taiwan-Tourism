5bc7e878e0bb9ddf8667f3d85164f5bba75bf0e0
taiRong225 <neribaby95260@gmail.com>
2021-12-02 11:53:36 +0800

    調整 ng build 編譯相關設置

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,16 @@
 export const environment = {
-  production: true
+  production: true,
+  baseURLv2: 'https://ptx.transportdata.tw/MOTC/v2',
+  baseURLv3: 'https://gist.motc.gov.tw/gist_api/v3',
+  APP_ID: '3c27fa835a8c492eaa6d21b89d9ab351',
+  APP_KEY: 'YauXGr2vbJtXzRDgJcTr7A2rX6w',
+  emptyString: '無',
+  noProvideCity: '未提供縣市',
+  noImage160x160: './assets/images/NoImage-160x160.png',
+  noImage250x200: './assets/images/NoImage-255x200.png',
+  noImage345x160: './assets/images/NoImage-345x160.png',
+  noImage1100x400: './assets/images/NoImage-1100x400.png',
+  page: 0,
+  perPage: 4,
+  distanceInMeters: 1000
 };
