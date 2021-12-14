a848bdf7b58a1fc453b18b95d98e14d5bfe222c7
taiRong225 <neribaby95260@gmail.com>
2021-12-14 11:04:41 +0800

    將取得縣市資料改為抓json檔案

--- src/app/services/map.service.ts
+++ src/app/services/map.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
+// import { environment } from 'src/environments/environment';
 import { City } from '../models/map.model';
 
 @Injectable({
@@ -10,20 +10,20 @@ import { City } from '../models/map.model';
 export class MapService {
 
   /** baseURL */
-  private baseURL: string;
+  // private baseURL: string;
 
   constructor(private http: HttpClient) {
-    this.baseURL = `${environment.baseURLv3}/Map`;
+    // this.baseURL = `${environment.baseURLv3}/Map`;
   }
 
   /**
    * 取得縣市資料
-   * 
    *
    * @return {*}  {Observable<City[]>}
    * @memberof MapService
    */
   getCities(): Observable<City[]> {
-    return this.http.get<City[]>(`${this.baseURL}/Basic/City`);
+    // return this.http.get<City[]>(`${this.baseURL}/Basic/City`);
+    return this.http.get<City[]>('./assets/database/city.json');
   }
 }
